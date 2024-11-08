65e8467491737d5657b516443c2445b3a9b36c2f
Levente Farkas <levente.farkas.256@gmail.com>
2024-11-08 20:59:18 +0100

    Login Form Component + Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginForm from "./components/LoginForm.jsx";
+import AdminPanel from "./components/AdminPanel.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/admin",
+    element: <AdminPanel></AdminPanel>,
+  },
+  {
+    path: "/login",
+    element: <LoginForm></LoginForm>,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
